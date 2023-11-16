d75c49e4da30e36c51dda22b95be48719d239892
Scrubbs <damonmurdoch1@gmail.com>
2023-11-16 15:12:51 +1000

    Fix nature in summary menu
    Fix nature in summary menu

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -1506,7 +1506,8 @@ static bool8 ExtractMonDataToSummaryStruct(struct Pokemon *mon)
     case 2:
         if (sMonSummaryScreen->monList.mons == gPlayerParty || sMonSummaryScreen->mode == SUMMARY_MODE_BOX || sMonSummaryScreen->handleDeoxys == TRUE)
         {
-            sum->nature = GetNature(mon, FALSE);
+            // [Ghoulslash] Nature mints implementation
+            sum->nature = GetNature(mon, TRUE);
             sum->currentHP = GetMonData(mon, MON_DATA_HP);
             sum->maxHP = GetMonData(mon, MON_DATA_MAX_HP);
             sum->atk = GetMonData(mon, MON_DATA_ATK);
@@ -1517,7 +1518,8 @@ static bool8 ExtractMonDataToSummaryStruct(struct Pokemon *mon)
         }
         else
         {
-            sum->nature = GetNature(mon, FALSE);
+            // [Ghoulslash] Nature mints implementation
+            sum->nature = GetNature(mon, TRUE);
             sum->currentHP = GetMonData(mon, MON_DATA_HP);
             sum->maxHP = GetMonData(mon, MON_DATA_MAX_HP);
             sum->atk = GetMonData(mon, MON_DATA_ATK2);
