e8afc317a93e31dc1b6419236382752900547608
Scrubbs <damonmurdoch1@gmail.com>
2023-11-26 19:12:02 +1000

    Fixed several bugs with .pory scripts, strings, shop content
    Fixed several bugs with .pory scripts, strings, shop content

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1502,10 +1502,10 @@
 
 #define FLAG_RECEIVED_POKEDEX_FROM_BIRCH            (SYSTEM_FLAGS + 0x84)
 
-#define FLAG_UNUSED_0x8E5                           (SYSTEM_FLAGS + 0x85) // Unused Flag
-#define FLAG_UNUSED_0x8E6                           (SYSTEM_FLAGS + 0x86) // Unused Flag
-#define FLAG_UNUSED_0x8E7                           (SYSTEM_FLAGS + 0x87) // Unused Flag
-#define FLAG_UNUSED_0x8E8                           (SYSTEM_FLAGS + 0x88) // Unused Flag
+#define FLAG_ITEM_MEGA_RING_OBTAINED                (SYSTEM_FLAGS + 0x85) // Unused Flag
+#define FLAG_ITEM_Z_CRYSTAL_OBTAINED                (SYSTEM_FLAGS + 0x86) // Unused Flag
+#define FLAG_ITEM_DYNAMAX_BAND_OBTAINED             (SYSTEM_FLAGS + 0x87) // Unused Flag
+#define FLAG_ITEM_TERA_ORB_OBTAINED                 (SYSTEM_FLAGS + 0x88) // Unused Flag
 #define FLAG_UNUSED_0x8E9                           (SYSTEM_FLAGS + 0x89) // Unused Flag
 #define FLAG_UNUSED_0x8EA                           (SYSTEM_FLAGS + 0x8A) // Unused Flag
 #define FLAG_UNUSED_0x8EB                           (SYSTEM_FLAGS + 0x8B) // Unused Flag
