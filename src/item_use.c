57f49c66583fac1fc7906e73762fa61f970c1d3a
Damon Murdoch <damonmurdoch1@gmail.com>
2023-12-01 00:50:08 +1000

    Fixed missing callback

--- src/item_use.c
+++ src/item_use.c
@@ -1383,6 +1383,7 @@ void ItemUseOutOfBattle_CannotUse(u8 taskId)
 void ItemUseOutOfBattle_Mints(u8 taskId)
 {
     gItemUseCB = ItemUseCB_Mints;
+    SetUpItemUseCallback(taskId);
 }
 
 void ItemUseOutOfBattle_ReduceIV(u8 taskId)
