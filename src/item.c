5d50ec324c68d2e17b18e51c15757991800ec506
Damon Murdoch <damonmurdoch1@gmail.com>
2024-01-17 22:23:04 +1000

    Reverted string changes
    Reverted string changes

--- src/item.c
+++ src/item.c
@@ -20,8 +20,6 @@
 
 static bool8 CheckPyramidBagHasItem(u16 itemId, u16 count);
 static bool8 CheckPyramidBagHasSpace(u16 itemId, u16 count);
-static const u8 *ItemId_GetPluralName(u16);
-static bool32 DoesItemHavePluralName(u16);
 
 EWRAM_DATA struct BagPocket gBagPockets[POCKETS_COUNT] = {0};
 
@@ -97,12 +95,6 @@ void CopyItemNameHandlePlural(u16 itemId, u8 *dst, u32 quantity)
         else
             StringCopy(dst, gText_PokeBalls);
     }
-
-    if (quantity < 2)
-        return;
-
-    if (DoesItemHavePluralName(itemId))
-        StringCopy(dst, ItemId_GetPluralName(itemId));
     else
         StringCopy(dst, ItemId_GetName(itemId));
     
@@ -901,16 +893,6 @@ const u8 *ItemId_GetName(u16 itemId)
 u32 ItemId_GetPrice(u16 itemId)
 {
     return gItems[SanitizeItemId(itemId)].price;
-}
-
-static bool32 DoesItemHavePluralName(u16 itemId)
-{
-    return (gItems[SanitizeItemId(itemId)].pluralName[0] != '\0');
-}
-
-static const u8 *ItemId_GetPluralName(u16 itemId)
-{
-    return gItems[SanitizeItemId(itemId)].pluralName;
 }
 
 const u8 *ItemId_GetEffect(u32 itemId)
