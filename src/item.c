3f03cbb2cd0ddf94aebe37bdd59f10d003509635
Damon Murdoch <damonmurdoch1@gmail.com>
2024-01-20 01:46:53 +1000

    Stripped unnecessary plural str code
    Stripped unnecessary plural str code

--- src/item.c
+++ src/item.c
@@ -84,27 +84,10 @@ void CopyItemName(u16 itemId, u8 *dst)
     StringCopy(dst, ItemId_GetName(itemId));
 }
 
-const u8 sText_s[] =_("s");
-
 void CopyItemNameHandlePlural(u16 itemId, u8 *dst, u32 quantity)
 {
-    if (itemId == ITEM_POKE_BALL)
-    {
-        if (quantity < 2)
-            StringCopy(dst, ItemId_GetName(ITEM_POKE_BALL));
-        else
-            StringCopy(dst, gText_PokeBalls);
-    }
-    else
-        StringCopy(dst, ItemId_GetName(itemId));
-    
-    if (quantity > 1)
-    {
-        if (itemId >= FIRST_BERRY_INDEX && itemId <= LAST_BERRY_INDEX)
-            GetBerryCountString(dst, gBerries[itemId - FIRST_BERRY_INDEX].name, quantity);
-        else
-            StringCopy(dst, ItemId_GetName(itemId));
-    }
+    // "[obtained/received] [item name] x[N]!"
+    CopyItemName(itemId, dst);
 }
 
 bool8 IsBagPocketNonEmpty(u8 pocket)
