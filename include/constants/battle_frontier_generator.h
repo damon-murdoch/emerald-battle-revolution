a97e09a31539352f3469d07d0b6ace05360f5529
Scrubbs <damonmurdoch1@gmail.com>
2024-02-15 19:44:34 +1000

    Working on simplified move selection algorithm
    Working on simplified move selection algorithm

--- include/constants/battle_frontier_generator.h
+++ include/constants/battle_frontier_generator.h
@@ -2,8 +2,11 @@
 #define GUARD_CONSTANTS_BATTLE_FRONTIER_GENERATOR_H
 
 enum {
-    BFG_MOVE_SELECT_DEFAULT, 
-    BFG_MOVE_SELECT_RANDOM
+    BFG_MOVE_SELECT_DEFAULT,                        // Default moves (Only replace if required)
+    BFG_MOVE_SELECT_RANDOM,                    // Select moves completely randomly (From Level Up / Teachable Moves)
+    BFG_MOVE_SELECT_FILTERED,
+    BFG_MOVE_SELECT_FILTERED_ATTACKS_ONLY,
+    BFG_MOVE_SELECT_VARIABLE                        // Switch based on VAR()
 };
 
 #endif // GUARD_CONSTANTS_BATTLE_FRONTIER_GENERATOR_H
